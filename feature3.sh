b43b481f5088a828847e0344c9dc818bb3935d39
jihyouk <ji091138@naver.com>
2025-12-03 16:11:13 +0900

    feature3-final
    ---
    Environment Info:
    BUILD_VERSION: test 1

--- feature3.sh
+++ feature3.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-
+#실행하기전 
+#cp feature3.sh .git/hooks/prepare-commit-msg
+#chmod +x .git/hooks/prepare-commit-msg
+#export BUILD_VERSION="test1"
 COMMIT_MSG_FILE=$1 #첫 번째 인자
 
 if [ -n "$BUILD_VERSION" ]; then #환경변수 비어있는지 확인
